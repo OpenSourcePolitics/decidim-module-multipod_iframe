f5850f3d59e8530580fcc4591f66a4b704021a9f
stephanie rousset <strabon@club-internet.fr>
2026-05-05 15:42:24 +0200

    test: setup and add tests

---
+++ spec/helpers/iframe_proposal_helper_spec.rb
@@ -0,0 +1,41 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+module Decidim
+  module MultipodIframe
+    describe IframeProposalHelper do
+      let(:component) { create(:proposal_component) }
+      let!(:proposal) { create(:proposal, :published, component:) }
+
+      before do
+        helper.class.include Decidim::SanitizeHelper
+        allow(helper).to receive(:current_component).and_return(component)
+      end
+
+      describe "iframe_src" do
+        let!(:string) { decidim_sanitize_translated(proposal.title) + decidim_sanitize_translated(proposal.body)}
+
+        context "when there is no query_params added in the iframe url" do
+          before do
+            component.update!(settings: { enable_iframe: true, iframe_url: "https://api.multipod.weblyzard.com" })
+          end
+
+          it "returns the url with similarto as query_params" do
+            expect(helper.iframe_src(proposal)).to eq("https://api.multipod.weblyzard.com?similarto=#{CGI.escape(string)}")
+          end
+        end
+
+        context "when there is a query_params added in the iframe url" do
+          before do
+            component.update!(settings: { enable_iframe: true, iframe_url: "https://api.multipod.weblyzard.com?date=2026-01-01,2026-04-30" })
+          end
+
+          it "adds similarto to the query_params list" do
+            expect(helper.iframe_src(proposal)).to eq("https://api.multipod.weblyzard.com?date=2026-01-01,2026-04-30&similarto=#{CGI.escape(string)}")
+          end
+        end
+      end
+    end
+  end
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,3 +7,25 @@ ENV["ENGINE_ROOT"] = File.dirname(__dir__)
 Decidim::Dev.dummy_app_path = File.expand_path(File.join("spec", "decidim_dummy_app"))
 
 require "decidim/dev/test/base_spec_helper"
+
+RSpec.configure do |config|
+  config.before(:each, type: :system) do
+    driven_by(:selenium_chrome_headless)
+
+    domain = (try(:organization) || try(:current_organization))&.host
+    if domain
+      # JavaScript sets the cookie also for all subdomains but localhost is a special case
+      domain = ".#{domain}" unless domain == "localhost"
+
+      page.driver.browser.execute_cdp(
+        "Network.setCookie",
+        domain: domain,
+        name: Decidim.consent_cookie_name,
+        value: { essential: true, analytics: true, marketing: true, preferences: true }.to_json,
+        path: "/",
+        expires: 1.day.from_now.to_i,
+        same_site: "Lax"
+      )
+    end
+  end
+end
