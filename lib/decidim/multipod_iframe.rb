b0f732ce4e7e26290f6410b6df41d50d807cc97b
stephanie rousset <strabon@club-internet.fr>
2026-05-05 10:54:30 +0200

    feat: add deface

--- lib/decidim/multipod_iframe.rb
+++ lib/decidim/multipod_iframe.rb
@@ -8,5 +8,10 @@ module Decidim
   # This namespace holds the logic of the `MultipodIframe` component. This component
   # allows users to create multipod_iframe in a participatory space.
   module MultipodIframe
+    include ActiveSupport::Configurable
+
+    config_accessor :deface_enabled do
+      ENV.fetch("DEFACE_ENABLED", nil) == "true" || Rails.env.test?
+    end
   end
 end
