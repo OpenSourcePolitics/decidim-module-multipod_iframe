ca8c7864b653fbcab216efafc3ab4fc411861b0e
stephanie rousset <strabon@club-internet.fr>
2026-05-05 16:28:27 +0200

    test: update version test

--- spec/lib/decidim/multipod_iframe/version_spec.rb
+++ spec/lib/decidim/multipod_iframe/version_spec.rb
@@ -6,8 +6,12 @@ module Decidim
   describe MultipodIframe do
     subject { described_class }
 
+    it "has decidim version" do
+      expect(subject.decidim_version).to eq("~> 0.29")
+    end
+
     it "has version" do
-      expect(subject.version).to eq("0.29.5")
+      expect(subject.version).to eq("1.0.0")
     end
   end
 end
