57dfb1f4a9a64cd0df4c5ce25316ba38ce0cc26a
stephanie rousset <strabon@club-internet.fr>
2026-05-06 10:22:38 +0200

    refactor: interpolation in iframe helper

--- app/helpers/decidim/multipod_iframe/iframe_proposal_helper.rb
+++ app/helpers/decidim/multipod_iframe/iframe_proposal_helper.rb
@@ -4,7 +4,7 @@ module Decidim
   module MultipodIframe
     module IframeProposalHelper
       def iframe_src(proposal)
-        content = decidim_sanitize_translated(proposal.title) + decidim_sanitize_translated(proposal.body)
+        content = "#{decidim_sanitize_translated(proposal.title)}. #{decidim_sanitize_translated(proposal.body)}"
         content = CGI.escape(content).first(1000)
         url = current_component.settings.iframe_url
         url.include?("?") ? url + "&similarto=#{content}" : url + "?similarto=#{content}"
